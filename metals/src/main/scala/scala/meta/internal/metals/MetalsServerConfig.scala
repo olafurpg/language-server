4cc955666a2ab4dd6939f2ad2addc61baac6634b
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-11-19 21:10:20 +0100

    More strict config parsing

--- metals/src/main/scala/scala/meta/internal/metals/MetalsServerConfig.scala
+++ metals/src/main/scala/scala/meta/internal/metals/MetalsServerConfig.scala
@@ -2,10 +2,10 @@ package scala.meta.internal.metals
 
 case class MetalsServerConfig(
     isExtensionsEnabled: Boolean =
-      System.getProperty("metals.extensions") != null,
+      System.getProperty("metals.extensions") != "true",
     bloopProtocol: BloopProtocol = BloopProtocol.Auto,
     isHttpEnabled: Boolean =
-      System.getProperty("metals.http") != null
+      System.getProperty("metals.http") != "true"
 )
 object MetalsServerConfig {
   def default: MetalsServerConfig = MetalsServerConfig()
