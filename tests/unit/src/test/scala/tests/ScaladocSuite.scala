caf48942567867935aeebaa26e163435f4d2ef87
Ólafur Páll Geirsson <olafurpg@gmail.com>
2019-02-02 17:18:57 +0100

    Add fork of Scaladoc parser in the compiler

--- tests/unit/src/test/scala/tests/ScaladocSuite.scala
+++ tests/unit/src/test/scala/tests/ScaladocSuite.scala
@@ -1,18 +1,19 @@
 package tests
 
+import scala.language.postfixOps
 import scala.meta._
-import scala.meta.contrib.ScaladocParser
+
 object Scaladoc {
   def toMarkdown(code: String): String = {
     val docstrings = code.tokenize.get.collect {
       case c: Token.Comment if c.syntax.startsWith("/**") =>
-        ScaladocParser.parseScaladoc(c)
+        ScaladocParser.parseAtSymbol(c.syntax)
     }
     pprint.log(docstrings)
-    docstrings.last
     code
   }
 }
+
 object ScaladocSuite extends BaseSuite {
 
   def check(name: String, original: String, expected: String): Unit = {
@@ -23,14 +24,26 @@ object ScaladocSuite extends BaseSuite {
   }
 
   check(
-    "define",
+    "tags",
     """
       |/**
-      | * @define description A number
+      | *  implicits for converting to and from `Int`.
+      | *
+      | *  ==Overview==
+      | *  The main class to use is [[my.package.complex.Complex]], as so
+      | *  {{{
+      | *  scala> val complex = Complex(4,3)
+      | *  complex: my.package.complex.Complex = 4 + 3i
+      | *  }}}
+      | *
+      | *  If you include [[my.package.complex.ComplexConversions]], you can
+      | *  convert numbers more directly
+      | *  {{{
+      | *  scala> import my.package.complex.ComplexConversions._
+      | *  scala> val complex = 4 + 3.i
+      | *  complex: my.package.complex.Complex = 4 + 3i
+      | *  }}}
       | */
-      |/**
-      |  * @param a $description
-      |  */
       |}
     """.stripMargin,
     """
