111ccc7964c7508996ca6989c2d8fa5b45beb517
Ólafur Páll Geirsson <olafurpg@gmail.com>
2019-02-04 14:00:25 +0100

    Reorder based on named arguments.

--- test-workspace/src/main/scala/example/User.scala
+++ test-workspace/src/main/scala/example/User.scala
@@ -3,8 +3,16 @@ package example
 // import Java
 
 object User {
-  class Foo[A] {
-    def add[B <: A](a: A, e: B): Nothing = ???
-  }
-  Map.empty[Int, String].applyOrElse(1, (a: Int) => "")
+  case class User(
+      name: String = "John",
+      age: Int = 42,
+      address: String = "Street",
+      country: String = "is"
+  )
+  User(age = 42)
+  // Map.empty[Int, String].applyOrElse[]
+
+  def user(name: String, age: Int): Int = age
+  def user(name: String, age: Int, street: Int): Int = age
+  // user(age = 1, street)
 }
