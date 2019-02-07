f1ef6eb7e36b8db0edc08228c943fad097361879
Ólafur Páll Geirsson <olafurpg@gmail.com>
2019-02-07 10:10:50 +0100

    WIP

--- metals-bench/src/main/scala/bench/CompletionBench.scala
+++ metals-bench/src/main/scala/bench/CompletionBench.scala
@@ -1,5 +1,6 @@
 package bench
 
+import java.nio.file.Path
 import java.util.concurrent.TimeUnit
 import org.openjdk.jmh.annotations.Benchmark
 import org.openjdk.jmh.annotations.BenchmarkMode
@@ -11,17 +12,48 @@ import org.openjdk.jmh.annotations.State
 import org.openjdk.jmh.annotations.TearDown
 import scala.meta.internal.metals.ClasspathSearch
 import scala.meta.internal.pc.ScalaPC
+import scala.meta.io.AbsolutePath
 import scala.meta.pc.CompletionItems
+import scala.meta.pc.PC
 import tests.Library
 import tests.SimpleJavaSymbolIndexer
 
+abstract class BaseCompletionBench {
+  var libraries: List[Library] = Nil
+  def downloadLibraries(): Unit = {
+    libraries = Library.jdk :: Library.all
+  }
+
+  def classpath: List[Path] =
+    libraries.flatMap(_.classpath.entries.map(_.toNIO))
+  def sources: List[AbsolutePath] = libraries.flatMap(_.sources.entries)
+
+  def newSearch(): ClasspathSearch = {
+    require(libraries.nonEmpty)
+    ClasspathSearch.fromClasspath(classpath, _ => 0)
+  }
+
+  def newIndexer() = new SimpleJavaSymbolIndexer(sources)
+
+  def newPC(
+      search: ClasspathSearch = newSearch(),
+      indexer: SimpleJavaSymbolIndexer = newIndexer()
+  ): ScalaPC = {
+    new ScalaPC(classpath, Nil, indexer, search)
+  }
+
+  def scopeComplete(pc: PC): CompletionItems = {
+    val code = "import Java\n"
+    pc.complete("A.scala", code, code.length - 2)
+  }
+}
+
 @State(Scope.Benchmark)
-class CompletionBench {
-  var libraries: List[Library] = _
+class OnDemandCompletionBench extends BaseCompletionBench {
 
   @Setup
   def setup(): Unit = {
-    libraries = Library.jdk :: Library.all
+    downloadLibraries()
   }
 
   @TearDown
@@ -31,13 +63,49 @@ class CompletionBench {
   @BenchmarkMode(Array(Mode.SingleShotTime))
   @OutputTimeUnit(TimeUnit.MILLISECONDS)
   def run(): CompletionItems = {
-    val classpath = libraries.flatMap(_.classpath.entries.map(_.toNIO))
-    val sources = libraries.flatMap(_.sources.entries)
-    val search = ClasspathSearch.fromClasspath(classpath, _ => 0)
-    val indexer = new SimpleJavaSymbolIndexer(sources)
-    val pc = new ScalaPC(classpath, Nil, indexer, search)
-    val code = "import Java\n"
-    pc.complete("A.scala", code, code.length - 2)
+    scopeComplete(newPC(newSearch(), newIndexer()))
+  }
+}
+
+@State(Scope.Benchmark)
+class CachedSearchAndCompilerCompletionBench extends BaseCompletionBench {
+  var pc: PC = _
+
+  @Setup
+  def setup(): Unit = {
+    downloadLibraries()
+    pc = newPC()
   }
 
+  @TearDown
+  def teardown(): Unit = {}
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.SingleShotTime))
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  def run(): CompletionItems = {
+    scopeComplete(pc)
+  }
+}
+
+@State(Scope.Benchmark)
+class CachedSearchCompletionBench extends BaseCompletionBench {
+  var pc: PC = _
+  var cachedSearch: ClasspathSearch = _
+
+  @Setup
+  def setup(): Unit = {
+    downloadLibraries()
+    cachedSearch = newSearch()
+  }
+
+  @TearDown
+  def teardown(): Unit = {}
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.SingleShotTime))
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  def run(): CompletionItems = {
+    scopeComplete(newPC(cachedSearch))
+  }
 }
--- metals-bench/src/main/scala/bench/MainBench.scala
+++ metals-bench/src/main/scala/bench/MainBench.scala
@@ -2,7 +2,7 @@ package bench
 
 object MainBench {
   def main(args: Array[String]): Unit = {
-    val bench = new CompletionBench
+    val bench = new OnDemandCompletionBench
     bench.setup()
     pprint.log(bench.run())
   }
