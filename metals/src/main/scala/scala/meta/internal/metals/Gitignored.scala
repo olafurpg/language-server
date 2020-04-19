3ee67e02e358db400c458a3fb57b80c586080b3d
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-04-19 10:39:05 +0000

    WIP

--- metals/src/main/scala/scala/meta/internal/metals/Gitignored.scala
+++ metals/src/main/scala/scala/meta/internal/metals/Gitignored.scala
@@ -1,11 +1,9 @@
 package scala.meta.internal.metals
 
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{Future => Try}
-import scala.util._
-import scala.util.{Try => _}
+import scala.util.{Try => _, _}
 
 object Gitignored {
-  Try(42)
+  Try.successful(42)
   Failure(new Exception())
 }
